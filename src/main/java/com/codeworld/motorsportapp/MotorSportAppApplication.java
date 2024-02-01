6b7a25d1c8a6d69218bcc3aea62c30901e192a34
katlegoprince1998 <princekatlego357@gmail.com>
2024-02-01 03:58:22 +0200

    Project files

---
+++ src/main/java/com/codeworld/motorsportapp/MotorSportAppApplication.java
@@ -0,0 +1,13 @@
+package com.codeworld.motorsportapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotorSportAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotorSportAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeworld/motorsportapp/MotorSportAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeworld.motorsportapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorSportAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
